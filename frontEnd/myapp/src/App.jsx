7e361232370ba54aa7a51f5fc4fc64f18c312a73
Ghada000 <assidighada4@gmail.com>
2024-01-24 15:10:12 +0100

    front

--- frontEnd/myapp/src/App.jsx
+++ frontEnd/myapp/src/App.jsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import PlantCard from './components/PlantCard.jsx'; // Assuming you have a PlantCard component
-import '../src/App.css'
+import React from 'react'
+import Nav from './components/Nav.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <nav className="navbar">
-        <div className="store-name">Smart Store</div>
-        <div className="nav-icons">
-          <div className="nav-icon" >
-            <i className="fas fa-plus"></i> Create
-          </div>
-          <div className="nav-icon">
-            <i className="fas fa-search"></i>
-            <input type="text" placeholder="Search products..." className="search-input" />
-          </div>
-        </div>
-      </nav>
-      <PlantCard />
+    <div>
+
+
+
+
+      <Nav/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
 
